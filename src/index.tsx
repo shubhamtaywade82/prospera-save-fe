dc665c25e28f24e3e09adb88d7d936a739964b95
Shubham Taywdae <shubhamtaywdae82@gmail.com>
2023-10-29 02:02:59 +0530

    Initial HelloWorld Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from "./app";
+import initI18n from './lang';
+import reportWebVitals from "./util/web-vitals";
+import "./styles/main.scss";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const htmlRoot = document.getElementById("root") as HTMLElement;
+const reactRoot = ReactDOM.createRoot(htmlRoot);
+
+reactRoot.render(
+  process.env.REACT_APP_SCRICT_MODE === "true" ? (
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  ) : (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+);
+if (process.env.REACT_APP_ENV !== "production") {
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  // eslint-disable-next-line no-console
+  reportWebVitals(console.log);
+}
