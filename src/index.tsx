dac2d8bde670d323c664a18b6e220c110eb0273c
Shubham Taywdae <shubhamtaywdae82@gmail.com>
2023-10-29 16:08:07 +0530

    Update eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { StrictMode } from "react";
 
 import App from "./app";
-import initI18n from "./lang";
 import reportWebVitals from "./util/web-vitals";
 import "./styles/main.scss";
 
